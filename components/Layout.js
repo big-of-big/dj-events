6dbc4dd4149f90fce429b8c65ca634cd2f5dc2d7
big-of-big <60137763+big-of-big@users.noreply.github.com>
2021-10-05 06:18:07 +0900

    ヘッダーとフッターを作成

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,7 @@
 import Head from "next/head"
+import Header from "./Header"
+import Footer from "./Footer"
+import styles from "../styles/Layout.module.css"
 
 const Layout = ({ title, keywords, description, children }) => {
   return (
@@ -8,7 +11,11 @@ const Layout = ({ title, keywords, description, children }) => {
         <meta name="discription" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
-      {children}
+      <Header />
+      <div className={styles.container}>
+        {children}
+      </div>
+      <Footer />
     </>
   )
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,2 +1,11 @@
-const Events = () => <h1>Events Page</h1>
+import Layout from "../../components/Layout"
+
+const Events = () => {
+  return (
+    <Layout>
+      <h1>Events Page</h1>
+    </Layout>
+  )
+}
+
 export default Events
